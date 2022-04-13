1dbdca05fc26a722b0706063925a350bc4025d08
dgotlieb <dgotlieb8@gmail.com>
2022-04-13 14:10:00 -0400

    add second print

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,5 +3,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println();
+        System.out.println("123");
     }
 }
